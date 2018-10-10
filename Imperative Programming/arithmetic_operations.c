5e16f9cf5b9d8af78aa88e2738d285c3a59a103f
Ronell Salunke <37792939+ronell1292k@users.noreply.github.com>
2018-10-10 17:33:35 +0530

    Create arithmetic_operations.c

---
+++ Imperative Programming/arithmetic_operations.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+  {
+  int a,b,add,sub,mul,div,mod;
+  printf("Enter the values of a and b");
+  scanf("%d%d",&a,&b);
+  add=a+b;
+  sub=a-b;
+  mul=a*b;
+  div=a/b;
+  mod=a%b;
+  printf("Add=%d\n",add);
+  printf("Sub=%d\n",sub);
+  printf("Mul=%d\n",mul);
+  printf("Div=%d\n",div);
+  printd("Mod=%d\n",mod);
+  getch()
+  }
